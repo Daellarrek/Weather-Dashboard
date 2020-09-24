38acf3998da5185f57fc6f8b1cb9763438f57ffc
David Moler <tigermoler@gmail.com>
2020-09-23 19:42:00 -0700

    After searching for a city it appends to the middle jumbotron

--- Work/script.js
+++ Work/script.js
@@ -1,19 +1,46 @@
-let APIKey = "8eddff1e9a5f42081d61f3f5b768aa88";
+$("#searchbtn").click(function () {
 
-let cityInput = "";
+    let currentDate = moment().format('dddd, MMMM Do, YYYY');
 
-let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey;
+    let APIKey = "8eddff1e9a5f42081d61f3f5b768aa88";
 
-let userInput = cityInput
+    let cityInput = $("#city-input").val();
 
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-})
+    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey;
 
-    .then(function (response) {
 
-        console.log(queryUrl)
-        console.log(response)
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
 
-    });
+        .then(function (response) {
+
+            console.log(queryUrl)
+            console.log(response)
+
+            $("#cityname").append(response.name).append(currentDate);
+            $("<div></div>").appendTo("#cityinfo").text(response.main.temp - 273.15)* 1.80 + 32;
+            $("<div></div>").appendTo("#cityinfo").text(response.main.humidity);
+            $("<div></div>").appendTo("#cityinfo").text(response.wind.speed * 2.237);
+
+            
+
+            
+            
+
+
+
+
+
+
+
+
+
+
+        });
+
+});
+
+
+
